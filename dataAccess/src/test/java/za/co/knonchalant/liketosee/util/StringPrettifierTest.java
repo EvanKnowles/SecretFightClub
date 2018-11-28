f35ab12f6ae56d39359470b494a9674912e44f0e
John Devenish <johnd4@discovery.co.za>
2018-11-28 15:26:55 +0200

    Icons for items

--- dataAccess/src/test/java/za/co/knonchalant/liketosee/util/StringPrettifierTest.java
+++ dataAccess/src/test/java/za/co/knonchalant/liketosee/util/StringPrettifierTest.java
@@ -1,10 +1,12 @@
 package za.co.knonchalant.liketosee.util;
 
 import org.junit.Test;
+import za.co.knonchalant.liketosee.domain.fightclub.Item;
 
 import static org.junit.Assert.*;
 
-public class ItemTest {
+public class StringPrettifierTest
+{
   @Test
   public void testNames() {
     assertEquals("the box", StringPrettifier.prettify("the box"));
@@ -17,6 +19,17 @@ public class ItemTest {
     assertEquals("an Object", StringPrettifier.prettify("Object"));
     assertEquals("a thing", StringPrettifier.prettify("thing"));
     assertEquals("a Thing", StringPrettifier.prettify("Thing"));
+  }
+
+  @Test
+  public void testIcons() {
+    Item weapon = new Item();
+    weapon.setDamage(10);
+    Item healing = new Item();
+    healing.setDamage(-10);
+    String damagingIcon = StringPrettifier.itemIcon(weapon);
+    String healingIcon = StringPrettifier.itemIcon(healing);
+    assertNotEquals(healingIcon, damagingIcon);
   }
 
 }
