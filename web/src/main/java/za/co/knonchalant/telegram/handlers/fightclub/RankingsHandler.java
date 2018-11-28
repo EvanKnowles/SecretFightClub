404aa022620995e5dd4f322655a10965f300a48e
John Devenish <johnd4@discovery.co.za>
2018-11-28 20:35:20 +0200

    Added trophy icon for player with most wins

--- web/src/main/java/za/co/knonchalant/telegram/handlers/fightclub/RankingsHandler.java
+++ web/src/main/java/za/co/knonchalant/telegram/handlers/fightclub/RankingsHandler.java
@@ -14,6 +14,9 @@ import java.util.List;
  * Created by evan on 2016/04/08.
  */
 public class RankingsHandler extends BaseMessageHandler {
+    private static final String SKULL = "\uD83D\uDC80";
+    private static final String TROPHY = "\uD83C\uDFC6";
+
     public RankingsHandler(String botName, IBotAPI bot) {
         super(botName, "rankings", bot, true);
     }
@@ -28,13 +31,19 @@ public class RankingsHandler extends BaseMessageHandler {
         List<Fighter> fightersInRoom = FighterDAO.get().findFightersInRoom(update.getChatId());
         fightersInRoom.sort(Comparator.comparing(Fighter::getHealth).reversed());
 
+        long mostWins = determineMostWins(fightersInRoom);
+
         StringBuilder stringBuilder = new StringBuilder("*Top fighters in Secret Fight Club*\n");
         for (Fighter fighter : fightersInRoom) {
             stringBuilder.append(fighter.getName());
             if (fighter.getHealth() <= 0) {
-                stringBuilder.append(" - technically dead");
+                stringBuilder.append(" " + SKULL);
             } else {
                 stringBuilder.append(" - ").append(fighter.getHealth()).append(" health");
+            }
+
+            if (fighter.getWins() == mostWins) {
+                stringBuilder.append(" ").append(TROPHY);
             }
 
             if (fighter.getWins() > 0) {
@@ -46,5 +55,15 @@ public class RankingsHandler extends BaseMessageHandler {
         sendMessage(update, stringBuilder.toString());
 
         return null;
+    }
+
+    private long determineMostWins(List<Fighter> fightersInRoom) {
+        long mostWins = 0;
+        for (Fighter fighter : fightersInRoom) {
+            if (fighter.getWins() > mostWins) {
+                mostWins = fighter.getWins();
+            }
+        }
+        return mostWins;
     }
 }
